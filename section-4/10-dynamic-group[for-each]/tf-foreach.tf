2635026fb02f4fe0c52b2361e37ecb3604118615
Soe Naing Htet <soenaing.htet@gmail.com>
2026-01-16 21:03:56 +0800

    validation

--- section-4/10-dynamic-group[for-each]/tf-foreach.tf
+++ section-4/10-dynamic-group[for-each]/tf-foreach.tf
@@ -11,3 +11,14 @@ resource "aws_iam_user" "dynamic_users" {
   for_each = toset(var.iam_users)
   name     = each.value
 }
+
+variable "db_password" {
+  type        = string
+  description = "Password for the database"
+
+  validation {
+    condition     = length(var.db_password) >= 12
+    error_message = "Database password length must be at least 12 characters long."
+  }
+
+}
